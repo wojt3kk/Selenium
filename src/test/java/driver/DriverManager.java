93641b7a44efe143d957ec49faae9e64ee03ca2b
Wojciech Karpowicz <wkarpowicz@e-point.pl>
2022-12-28 15:41:08 +0100

    Initial commit

--- src/test/java/driver/DriverManager.java
+++ src/test/java/driver/DriverManager.java
@@ -4,26 +4,42 @@ import org.openqa.selenium.WebDriver;
 
 import static configuration.TestRunProperties.getBrowserToRun;
 import static configuration.TestRunProperties.getIsRemoteRun;
+import static driver.BrowserType.FIREFOX;
 
 public class DriverManager {
-        private static WebDriver driver;
+        //private static WebDriver driver;
+
+        //Utworzenie zmiennej webDriverThreadLocal
+        private static ThreadLocal<WebDriver> webDriverThreadLocal = new ThreadLocal<>();
         private DriverManager(){
 
         }
 
         public static WebDriver getWebDriver(){
 
-            if (driver == null){
-                driver = new BrowserFactory(getBrowserToRun(), getIsRemoteRun()).getBrowser();
+            //Sprawdzenie czy wartość zmiennej WebDrivera dla danego wątku jest nullem
+            if (webDriverThreadLocal.get() == null){
+
+                //Wywołanie metody getBrowser() z klasy BrowserFactory zwraca instancję WebDrivera, który następnie jest
+                // dodana do puli instancji klasy ThreadLocal za pomocą metody set()
+                webDriverThreadLocal.set(new BrowserFactory(getBrowserToRun(),getIsRemoteRun()).getBrowser());
             }
-            return driver;
+
+            //Zwrócenie instancji WebDrivera dla danego wątku
+            return webDriverThreadLocal.get();
         }
 
         public static void disposeDriver(){
-            driver.close();
-            if (!getBrowserToRun().equals(BrowserType.FIREFOX)){
-                driver.quit();
+
+            //Wywołanie metody close() z klasy WebDriver dla danego wątku
+            webDriverThreadLocal.get().close();
+            if (!getBrowserToRun().equals(FIREFOX)){
+
+                //Wywołanie metody quit() z klasy WebDriver dla danego wątku
+                webDriverThreadLocal.get().quit();
             }
-            driver = null;
+
+            //Wywołanie metody remove() z klasy ThreadLocal dla danego wątku w celu usunięcia WebDrivera dla aktualnego wątku
+            webDriverThreadLocal.remove();
         }
 }
